bf32d968a83baba0f8c86d6a2ea6f537d30392d3
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-08-02 16:09:13 +0800

    Cleanup import blocks

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -35,20 +35,3 @@ resource "cloudflare_dns_record" "www" {
   content   = "nixos-cn.github.io"
   zone_id = cloudflare_zone.nixos_cn_org.id
 }
-
-# reimport all resources
-
-import {
-  to = cloudflare_zone.nixos_cn_org
-  id = "75b1fd9c9c27d1383681f44b596b171c"
-}
-
-import {
-  to = cloudflare_dns_record.apex
-  id = "75b1fd9c9c27d1383681f44b596b171c/1d147a30122f9caa4b29638fb5534f1c"
-}
-
-import {
-  to = cloudflare_dns_record.www
-  id = "75b1fd9c9c27d1383681f44b596b171c/4cd8a9ad716359b6fda7b93c7a373947"
-}

40c3e686eea625b4f3830ec9ca16b78b46bf692c
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-08-02 15:26:36 +0800

    Migrate to cloudflare provider 5

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -6,30 +6,32 @@ variable "cloudflare_account_id" {
 }
 
 resource "cloudflare_zone" "nixos_cn_org" {
-  account_id = var.cloudflare_account_id
-  zone       = "nixos-cn.org"
+  name = "nixos-cn.org"
+  account = {
+    id = var.cloudflare_account_id
+  }
 }
 
 # ttl = 1 for automatic
 
 # apex
 
-resource "cloudflare_record" "apex" {
+resource "cloudflare_dns_record" "apex" {
   name    = "nixos-cn.org"
   proxied = false
   ttl     = 1
   type    = "CNAME"
-  value   = "nixos-cn.github.io"
+  content   = "nixos-cn.github.io"
   zone_id = cloudflare_zone.nixos_cn_org.id
 }
 
 # www
 
-resource "cloudflare_record" "www" {
-  name    = "www"
+resource "cloudflare_dns_record" "www" {
+  name    = "www.nixos-cn.org"
   proxied = false
   ttl     = 1
   type    = "CNAME"
-  value   = "nixos-cn.github.io"
+  content   = "nixos-cn.github.io"
   zone_id = cloudflare_zone.nixos_cn_org.id
 }
